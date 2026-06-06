51c6d3ad141c284c2ac2c2b0a96a93350f83bdc2
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-06-06 08:35:38 +0600

    added token veriy functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const dotenv = require("dotenv")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-node-cjs-runtime');
 
 dotenv.config()
 
@@ -27,16 +28,42 @@ const client = new MongoClient(uri, {
   }
 });
 
+  const JWKS = createRemoteJWKSet(
+    new URL("http://localhost:3000/api/auth/jwks")
+  )
+
+const verifyToken = async(req, res , next)=>{
+  const authHeader = req?.headers.authorization
+  if(!authHeader){
+    return res.status(401).json({message: "Unauthorized"});
+  }
+  const token = authHeader.split(" ")[1];
+  if(!token){
+    return res.status(401).json({message: "Unauthorized"})
+  }
+try {
+   const {payload}  = await jwtVerify(token, JWKS)
+    // console.log(payload)
+    next()
+  
+} catch (error) {
+  return res.status(403).json({message: "Forbidden"})
+}
+
+
+
+}
 async function run() {
   try {
     
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("hireloop");
     const jobsCollection = database.collection("jobs");
 
     
-    app.post('/jobs', async(req, res)=>{
+    app.post('/jobs',verifyToken, async(req, res)=>{
+
         try {
             const newJob = req.body;
             const result = await jobsCollection.insertOne(newJob);
@@ -47,14 +74,14 @@ async function run() {
         }
     })
 
-    app.get('/jobs/:userId', async(req, res)=>{
+    app.get('/jobs/:userId',verifyToken, async(req, res)=>{
         const userId = req.params.userId;
         const query = {companyId: userId};
         const cursor  = jobsCollection.find(query);
         const result = await cursor.toArray();
         res.json(result);
     })
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally{
 
